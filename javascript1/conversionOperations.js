e9352ef94fc36e87b478e36413aec67ff141b8aa
Krutika-Dubey <krutikadubey412@gmail.com>
2024-05-27 05:23:56 +0000

    commit config done

---
+++ javascript1/conversionOperations.js
@@ -0,0 +1,26 @@
+// 
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue);
+
+// let str1 = "krutika"
+// let str2 = "dubey"
+
+// let fullName= str1+ " "+ str2
+// console.log(fullName)
+
+// console.log("1" + 2);
+// console.log("1"+2+2+"5");
+// console.log((+true));
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1= num2=num3=2+2
+// num1 = num2 = num3 = (3+4)*5%3
+// console.table([num1,num2,num3]);
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
