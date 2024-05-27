317b6ef6639d304cfdf508a3fdddafc2ed93b957
Krutika-Dubey <krutikadubey412@gmail.com>
2024-05-27 05:33:03 +0000

    commit config done

---
+++ javascript1/jslec2.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// alert(3+3)
+
+ console.log(3+3);  //code credibility should be high
+
+ console.log("Krutika");
+
+ let name="krutika"
+ let age = 30
+ let isLoggedIn = false
+ let state;
+
+//  console.log(typeof name);
